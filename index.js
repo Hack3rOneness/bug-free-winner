6248b566f7384cb404d94c01854e8f0822a0bff1
~Hack3r_Oneness <36328900+Hack3rOneness@users.noreply.github.com>
2021-08-15 18:34:03 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+// index.js
+
+// Require Puppeteer.
+const puppeteer = require("puppeteer");
+
+async function generatePDF() {
+  // Launch a new browser session.
+  const browser = await puppeteer.launch();
+  // Open a new Page.
+  const page = await browser.newPage();
+
+  // Go to our invoice page that we serve on `localhost:8000`.
+  await page.goto("http://localhost:8000");
+  // Store the PDF in a file named `invoice.pdf`.
+  await page.pdf({ path: "invoice.pdf", format: "A4" });
+
+  await browser.close();
+}
